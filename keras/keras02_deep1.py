51686a2e4c1cb657142653af8a2c22d2bc2adc6d
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2023-12-29 19:24:58 +0900

    코드 정리

--- keras/keras02_deep1.py
+++ keras/keras02_deep1.py
@@ -1,53 +1,40 @@
 from keras.models import Sequential
 from keras.layers import Dense
-import random
 import numpy as np
 
-
-resultt = []
+results = []
 
 def deep(a,b):
-
-#1 data
+    #1 data
     x = np.array([1,2,3])
     y = np.array([1,2,3])
-
-
-
 
     #2 modeling
     ###[실습] 레이어의 깊이와 노드의 갯수를 이용해 4를 만들어라
     model = Sequential()
-    
+       
     model.add(Dense(2, input_dim=1))
     for i in range(1, a):
         model.add(Dense(b))
     model.add(Dense(1))
-
-
-
+    
     #3 compile
     model.compile(loss="mse", optimizer="adam")
     model.fit(x,y, epochs=100)
-
-
     #4 evaluate
+    
     loss = model.evaluate(x,y)
     print("loss : ", loss)
     result = model.predict([4])
-    resultt.append(result)
+    results.append(result)
     print("result : ", result)
     print("a, b", a,b)
 
-# a = int(input("a"))
-# b = int(input("b"))
 for i in range(1, 100):
     deep(i,10)
-    
-idx = 1
-for i in resultt:
-    print(i, idx)
-    idx += 1
+
+for i in results:
+    print(i)
     
     
 # [[4.000989]] , [[3.9990513]]
