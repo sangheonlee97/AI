85fc6a66939d75195738b53126f244934d936d1e
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-01-03 14:51:18 +0900

    output layer = 3 실습

---
+++ keras/keras04_mlp3_2.py
@@ -0,0 +1,44 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+
+#1. data
+x = np.array([range(10), range(21,31), range(201, 211)])
+# print(x)
+print(x.shape)      # (3, 10)
+
+x = x.T
+# print(x)
+print(x.shape)      # (10, 3)
+
+y = np.array([range(1,11),
+              [1,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9],
+              range(9,-1,-1)
+             ])
+y = y.T
+
+# [실습]
+# 예측 : [10, 31, 211]
+
+#2. modeling
+model = Sequential()
+model.add(Dense(5,input_dim=3))
+model.add(Dense(10))
+model.add(Dense(10))
+model.add(Dense(8))
+model.add(Dense(4))
+model.add(Dense(3))
+
+#3. compile, train
+model.compile(loss='mse', optimizer='adam')
+model.fit(x, y, epochs=1000, batch_size=1)
+
+#4 evaluate, predict
+loss = model.evaluate(x,y)
+pred = model.predict([[10, 31, 211]])
+
+print("loss : ", loss)
+print("pred : ", pred)
+
+# loss :  5.4137978822144106e-11
+# pred :  [[10.999995   1.9999957 -0.9999852]]
