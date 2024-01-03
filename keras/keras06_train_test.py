efa3b01aab3e8c31c16f2baf306a5dad6da4c495
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-01-03 16:53:22 +0900

    데이터셋 분리 실습

--- keras/keras06_train_test.py
+++ keras/keras06_train_test.py
@@ -2,8 +2,14 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 
-x = np.array([1,2,3,4,5,6,7,8,9,10])
-y = np.array([1,2,3,4,6,5,7,8,9,10])
+# x = np.array([1,2,3,4,5,6,7,8,9,10])
+# y = np.array([1,2,3,4,6,5,7,8,9,10])
+
+x_train = np.array([1,2,3,4,5,6,7])
+y_train = np.array([1,2,3,4,6,5,7])
+
+x_test = np.array([8,9,10])
+y_test = np.array([8,9,10])
 
 model = Sequential()
 model.add(Dense(1,input_dim=1))
@@ -14,13 +20,13 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(x,y, epochs=1000, batch_size=1)
+model.fit(x_train,y_train, epochs=1000, batch_size=1)
 
-loss = model.evaluate(x,y)
+loss = model.evaluate(x_test,y_test)
 pred = model.predict([1100000, 7])
 
 print("loss : ", loss)
 print("pred : ", pred)
 
-# loss :  0.20051309466362
-# pred :  [[1.0928689e+06] [7.0285268e+00]]
+# loss :  0.2474503517150879
+# pred :  [[1.02295694e+06] [6.64593267e+00]]
