0721dc4950afcb9d79a74a2289601205e4b48b8c
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-01-08 14:22:21 +0900

    kaggle 따릉이 실습

---
+++ keras/keras10_kaggle_bike.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+path = "C:\\Study\\_data\\kaggle\\bike\\"
+
+
+# 1. data
+df_train = pd.read_csv(path + "train.csv", index_col=0)
+df_test = pd.read_csv(path + "test.csv", index_col=0)
+df_sub = pd.read_csv(path + "sampleSubmission.csv")
+# print(df_train.shape)   # (10886, 11)
+# print(df_test.shape)    # (6493, 8)
+# print(df_sub.shape)     # (6493, 2)
+
+# train df의 'casual', 'registered' 컬럼 삭제
+df_train = df_train.drop(['casual'], axis=1)
+df_train = df_train.drop(['registered'], axis=1)
+# print(df_train.shape)   # (10886, 9)
+
+# print(df_train.isna().sum()) # 0
+# print(df_test.isna().sum()) # 0
+
+# train df의 target 분리
+df_train_X = df_train.drop(['count'], axis=1)
+df_train_y = df_train['count']
+
+df_train_X_train, df_train_X_test, df_train_y_train, df_train_y_test = train_test_split(df_train_X, df_train_y, test_size=0.3, shuffle=0, random_state=42)
+
+# 2. modeling
+model = Sequential()
+model.add(Dense(20, input_dim=8))
+model.add(Dense(40))
+model.add(Dense(40))
+model.add(Dense(40))
+model.add(Dense(20))
+model.add(Dense(1))
+
+# 3. compile, fit
+model.compile(loss='mse', optimizer='adam')
+model.fit(df_train_X_train, df_train_y_train, epochs=200, batch_size=100)
+
+# 4. predict
+y_pred = model.predict(df_train_X_test)
+r2 = r2_score(df_train_y_test, y_pred)
+print("r2 score : ",r2)
+
+y_sub = model.predict(df_test)
+idx = 0
+for i in y_sub:
+    if i < 0:
+        y_sub[idx] = 0
+    idx = idx + 1
+
+df_sub['count'] = y_sub
+df_sub.to_csv(path + "submission.csv", index=False)
