1f428f7f99c1c4284c127be61d2f8147e0763248
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-01-09 16:26:52 +0900

    5가지 실습파일에 시각화
    plt

---
+++ keras/keras14_overfit2_california.py
@@ -0,0 +1,71 @@
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
+from keras.models import Sequential
+from keras.layers import Dense
+from sklearn.metrics import r2_score, accuracy_score
+import numpy as np
+import pandas as pd
+import time
+
+
+datasets = fetch_california_housing()
+
+# print(datasets.feature_names)       # ['MedInc', 'HouseAge', 'AveRooms', 'AveBedrms', 'Population', 'AveOccup', 'Latitude', 'Longitude']
+
+
+X = datasets.data
+y = datasets.target
+
+
+
+# [실습]
+# R2 0.55 ~ 0.6
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1228)
+
+model = Sequential()
+model.add(Dense(10, input_dim=8))
+model.add(Dense(30))
+model.add(Dense(15))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+
+
+
+start_time = time.time()
+
+hist = model.fit(X_train, y_train, epochs=500, batch_size=142, validation_split=0.3)
+
+end_time = time.time()
+
+y_pred = model.predict(X_test)
+
+loss = model.evaluate(X_test, y_test)
+
+r2 = r2_score(y_test, y_pred)
+
+print("loss : ", loss)
+print("r2 : ", r2)
+print("걸린 시간 : ", round(end_time - start_time,2), "초")
+
+# loss :  0.5542855858802795
+# r2 :  0.5767131444516536
+# random_state = 1228
+# epochs = 1000
+# batch_size = 142
+
+import matplotlib.pyplot as plt
+plt.rcParams['font.family'] ='Malgun Gothic'
+plt.rcParams['axes.unicode_minus'] =False
+
+plt.figure(figsize=(50, 30))
+plt.plot(hist.history['loss'], color='red', label='loss', marker='.')
+plt.plot(hist.history['val_loss'], color='blue', label='val_loss', marker='.')
+plt.xlabel('에폭')
+plt.title('캘리포니아 로스', fontsize=30)
+plt.ylabel('로스')
+plt.legend(loc = 'upper right')
+plt.grid()
+plt.ylim(0, 1000)
+plt.show()
