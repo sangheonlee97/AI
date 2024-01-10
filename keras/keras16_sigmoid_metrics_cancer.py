6abf34bc8058283b66a894fb23efbac9e13da61b
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-01-10 17:33:59 +0900

    dacon 당뇨병 대회 실습 acc(0.74)

--- keras/keras16_sigmoid_metrics_cancer.py
+++ keras/keras16_sigmoid_metrics_cancer.py
@@ -4,7 +4,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 from sklearn.model_selection import train_test_split
 from keras.callbacks import EarlyStopping
-from sklearn.metrics import r2_score
+from sklearn.metrics import r2_score, accuracy_score
 import pandas as pd
 #1. data
 datasets = load_breast_cancer()
@@ -26,7 +26,7 @@ a2 = np.where(y==1)
 print("0 : ", len(a1[0]) )
 print("1 : ", len(a2[0]) )
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=42)
 
 #2. model
 model = Sequential()
@@ -39,28 +39,25 @@ model.add(Dense(1, activation='sigmoid'))
 #3. compile, fit
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-es = EarlyStopping(monitor='val_accuracy', mode='max', patience=70, verbose=1, restore_best_weights=True)
+es = EarlyStopping(monitor='val_accuracy', mode='max', patience=300, verbose=1, restore_best_weights=True)
 
-model.fit(X_train, y_train, epochs=1000, validation_split=0.1, callbacks=[es])
+model.fit(X_train, y_train, epochs=10000, validation_split=0.15, callbacks=[es])
 
 #4. evaluate, pred
 y_pred = model.predict(X_test)
 
+y_pred = y_pred.round()
 
-
-idx=0
-for i in y_pred:
-    if 3-i > 2.5:
-        y_pred[idx] = 0
-    else:
-        y_pred[idx] = 1
-    idx += 1
 loss = model.evaluate(X_test, y_test)
 r2 = r2_score(y_test, y_pred)
 
+def ACC(a, b):
+    return accuracy_score(a,b)
+acc = ACC(y_test, y_pred)
 print(y_pred)  
 print("loss : ", loss)
 print("r2 : ", r2)
+print("acc : ", acc)
 
 
 
