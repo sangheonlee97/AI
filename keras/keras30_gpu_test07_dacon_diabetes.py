ea4e6ad77f20d928c57ed1f801cc5262906ca2c5
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-01-18 16:30:39 +0900

    지금까지 했던 모든 실습파일 gpu로 돌렸을 때 시간 기록

---
+++ keras/keras30_gpu_test07_dacon_diabetes.py
@@ -0,0 +1,139 @@
+
+import numpy as np
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error,accuracy_score
+from keras. callbacks import EarlyStopping,ModelCheckpoint
+from sklearn.preprocessing import MinMaxScaler, MaxAbsScaler, StandardScaler, RobustScaler
+path = "..//_data//dacon//diabetes//"
+
+train_csv = pd.read_csv(path + "train.csv", index_col=0)
+# print(train_csv)        #(652, 9)
+
+test_csv = pd.read_csv(path + "test.csv", index_col=0)
+# print(test_csv)         #(116, 8)
+
+submission_csv = pd.read_csv(path + "sample_submission.csv")
+# print(submission_csv)   #(116, 2)
+
+# train_csv['Glucose']
+# train_csv['Glucose'] = train_csv['Glucose'].fillna(train_csv['Glucose'].mean())
+
+# print(train_csv['Glucose'][train_csv['Glucose']==0].count())    # 4
+# print(train_csv['BloodPressure'][train_csv['BloodPressure']==0].count())    # 30
+# print(train_csv['SkinThickness'][train_csv['SkinThickness']==0].count())    # 195
+# print(train_csv['Insulin'][train_csv['Insulin']==0].count())    # 318
+# print(train_csv['BMI'][train_csv['BMI']==0].count())    # 7
+
+
+
+
+
+
+X = train_csv.drop(['Outcome'], axis=1)
+y = train_csv['Outcome']
+
+# print(X.shape, y.shape)     # (652, 8) (652)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, shuffle=True, random_state=713)
+
+################    MinMaxScaler    ##############################
+# mms = MinMaxScaler()
+# mms.fit(X_train)
+# X_train = mms.transform(X_train)
+# X_test = mms.transform(X_test)
+# test_csv =mms.transform(test_csv)
+
+################    StandardScaler    ##############################
+
+# sts = StandardScaler()
+# sts.fit(X_train)
+# X_train = sts.transform(X_train)
+# X_test = sts.transform(X_test)
+
+# print(X_train)
+# print(X_test)
+
+# ################    MaxAbsScaler    ##############################
+mas = MaxAbsScaler()
+mas.fit(X_train)
+X_train = mas.transform(X_train)
+X_test = mas.transform(X_test)
+test_csv = mas.transform(test_csv)
+
+# ################    RobustScaler    ##############################
+# rbs = RobustScaler()
+# rbs.fit(X_train)
+# X_train = rbs.transform(X_train)
+# X_test = rbs.transform(X_test)
+
+# print(X_train.shape, y_train.shape)         # (456, 8) (456,)
+# print(X_test.shape, y_test.shape)           # (196, 8) (196,)
+
+# model = Sequential()
+# model.add(Dense(19, input_dim = 8,activation='relu'))               
+# model.add(Dense(97))
+# model.add(Dense(9))
+# model.add(Dense(21))
+# model.add(Dense(3))
+# model.add(Dense(41))
+# model.add(Dense(1, activation='sigmoid'))
+from keras.layers import Input
+from keras.models import Model
+input = Input(shape=(8, ))
+den1 = Dense(19, activation='relu')(input)
+den2 = Dense(97, activation='relu')(den1)
+den3 = Dense(9)(den2)
+den4 = Dense(21, activation='relu')(den3)
+den5 = Dense(99, activation='relu')(den4)
+den6 = Dense(7)(den5)
+out = Dense(1,activation='sigmoid')(den6)
+model = Model(inputs=input, outputs=out)
+
+import datetime
+date = datetime.datetime.now()
+# print(date)                     # 2024-01-17 10:52:59.857389
+date = date.strftime("%m%d_%H%M")                   # _는 str      
+# print(date)                     # 0117_1058
+# print(type(date))               # <class 'str'>
+
+path = "..\\_data\\_save\\MCP\\"
+filename = '{epoch:05d}-{acc:.4f}-{loss:.4f}.hdf5'            # 04d : 4자리 정수표현, 4f : 소수4번째자리까지 표현, 예) 1000_0.3333.hdf5
+filepath = "".join([path, 'k26_dacon_diabetes_',date,'_', filename])
+
+mcp = ModelCheckpoint(monitor='val_loss', mode='min', verbose=1, save_best_only=True, filepath=filepath)    
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics='acc')
+es = EarlyStopping(monitor='val_loss' , mode='min', patience=1000, verbose=3, restore_best_weights=True)
+import time
+start_time = time.time()
+
+
+hist = model.fit(X_train, y_train, epochs=5000, batch_size=16, validation_split=0.15, callbacks=[es,mcp] )
+end_time = time.time()
+loss = model.evaluate(X_test, y_test)
+y_submit = model.predict(test_csv)
+
+# print(y_submit)
+# print(y_submit.shape)       #(196, 1)
+
+submission_csv['Outcome'] = y_submit.round()                                       
+# print(submission_csv)       #(116, 2)
+
+submission_csv.to_csv(path + "submission_0116_1_.csv", index=False)
+
+y_predict = model.predict(X_test)
+y_predict = y_predict.round()
+def ACC(aaa, bbb):
+    (accuracy_score(aaa, bbb))
+    return (accuracy_score(aaa, bbb))
+acc = ACC(y_test, y_predict)
+print("ACC : ", acc)
+print("로스 : ", loss)
+
+print("걸린 시간 : ", round(end_time - start_time,2), "초")
+
+
+# cpu : 36.8
+# gpu : 110
---
+++ keras/keras30_gpu_test09_fetch_covtype.py
@@ -0,0 +1,137 @@
+from sklearn.datasets import fetch_covtype
+import pandas as pd
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from keras.utils import to_categorical
+from sklearn.preprocessing import OneHotEncoder
+from keras. callbacks import EarlyStopping,ModelCheckpoint
+from sklearn.preprocessing import MinMaxScaler, MaxAbsScaler, StandardScaler, RobustScaler
+
+datasets = fetch_covtype()
+
+X = datasets.data
+y = datasets.target
+# y = y.reshape(-1, 1)
+# print(X.shape, y.shape)         # (581012, 54) (581012)
+# print(pd.value_counts(y))       # 7
+
+
+# 2    283301
+# 1    211840
+# 3     35754
+# 7     20510
+# 6     17367
+# 5      9493
+# 4      2747
+
+# 1. scikit-learn 방식
+# y = OneHotEncoder(sparse=False).fit_transform(y)
+# print(y.shape)      #(581012, 7)
+# print(y)            
+
+# 2. pandas 방식
+# y = pd.get_dummies(y)
+# print(y.shape)       #(581012, 7)
+
+
+# 3. keras 방식
+y = to_categorical(y)
+y= y[:,1:]                 # 행렬 슬라이싱
+# print(y.shape)          # (581012, 7)
+# print(y)
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.35, shuffle=True, random_state=3, stratify=y)
+
+################    MinMaxScaler    ##############################
+# mms = MinMaxScaler()
+# mms.fit(X_train)
+# X_train = mms.transform(X_train)
+# X_test = mms.transform(X_test)
+
+################    StandardScaler    ##############################
+
+# sts = StandardScaler()
+# sts.fit(X_train)
+# X_train = sts.transform(X_train)
+# X_test = sts.transform(X_test)
+
+# print(X_train)
+# print(X_test)
+
+# ################    MaxAbsScaler    ##############################
+# mas = MaxAbsScaler()
+# mas.fit(X_train)
+# X_train = mas.transform(X_train)
+# X_test = mas.transform(X_test)
+
+
+# ################    RobustScaler    ##############################
+rbs = RobustScaler()
+rbs.fit(X_train)
+X_train = rbs.transform(X_train)
+X_test = rbs.transform(X_test)
+
+# print(X_train.shape, X_test.shape)      # (464809, 54) (116203, 54)
+# print(y_train.shape, y_test.shape)      # (464809, 7) (116203, 7)
+
+
+# model = Sequential()
+# model.add(Dense(19, input_dim =54, activation='relu'))
+# model.add(Dense(97))
+# model.add(Dense(9))
+# model.add(Dense(21, activation='sigmoid'))
+# model.add(Dense(9))
+# model.add(Dense(97))
+# model.add(Dense(19))
+# model.add(Dense(7, activation='softmax'))
+from keras.models import Model
+from keras.layers import Input
+inp = Input(shape=(54, ))
+d1 = Dense(19, activation='relu')(inp)
+d2 = Dense(97)(d1)
+d3 = Dense(9, activation='sigmoid')(d2)
+d4 = Dense(21)(d3)
+op = Dense(7, activation='softmax')(d4)
+model = Model(inputs=inp, outputs=op)
+
+
+import datetime
+date = datetime.datetime.now()
+# print(date)                     # 2024-01-17 10:52:59.857389
+date = date.strftime("%m%d_%H%M")                   # _는 str      
+# print(date)                     # 0117_1058
+# print(type(date))               # <class 'str'>
+
+path = "..\\_data\\_save\\MCP\\"
+filename = '{epoch:05d}-{acc:.4f}-{loss:.4f}.hdf5'            # 04d : 4자리 정수표현, 4f : 소수4번째자리까지 표현, 예) 1000_0.3333.hdf5
+filepath = "".join([path, 'k26_9_fetch_covtype',date,'_', filename])
+
+mcp = ModelCheckpoint(monitor='val_loss', mode='min', verbose=1, save_best_only=True, filepath=filepath)    
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics='acc')
+# es = EarlyStopping(monitor='val_loss', mode='min', patience=100, restore_best_weights=True, verbose=1)
+
+import time
+start_time = time.time()
+
+
+hist = model.fit(X_train, y_train, epochs=1500, batch_size=1000, validation_split=0.1)
+end_time = time.time()
+results = model.evaluate(X_test, y_test)
+print("로스 : ", results[0])
+print("ACC : ", results[1])
+
+y_predict = model.predict(X_test)
+y_test = np.argmax(y_test, axis=1)
+y_predict = np.argmax(y_predict, axis=1)
+
+acc = accuracy_score(y_test, y_predict)
+print("accuracy_score : ", acc)
+
+print("걸린 시간 : ", round(end_time - start_time,2), "초")
+
+# cpu : 320
+# gpu : 563
