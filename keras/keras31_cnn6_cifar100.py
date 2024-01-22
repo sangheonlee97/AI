5bcdcc72bbe0839ddeccee6cdd872a4d1375efe9
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-01-22 14:51:32 +0900

    cifar100, mnist_fashion 예제 실습

--- keras/keras31_cnn6_cifar100.py
+++ keras/keras31_cnn6_cifar100.py
@@ -11,33 +11,48 @@ import matplotlib.pyplot as plt
 print(X_train.shape, X_test.shape) # (50000, 32, 32, 3) (10000, 32, 32, 3)
 # plt.imshow(X_train[2])
 # plt.show()
-# print(y_train.shape, y_test.shape) 
+print(y_train.shape, y_test.shape) 
 print(np.unique(y_train, return_counts=True)) # 0 ~ 9
 print(np.unique(y_test, return_counts=True)) # 0 ~ 9
 # print(X_train)
 # print(X_test)
-# print(y_train)
-# print(y_test)
+print(y_train.shape)
+print(y_test.shape)
 
 ohe = OneHotEncoder(sparse=False)
 y_train = ohe.fit_transform(y_train)
 y_test = ohe.transform(y_test)
 
+####################################
 # scaler = MinMaxScaler()
 # X_train = scaler.fit_transform(X_train)
 # X_test = scaler.transform(X_test)
-X_train = X_train / 255
-X_test = X_test / 255
+# X_train = X_train / 255
+# X_test = X_test / 255
+####################################
+
+####################################
+mean = np.mean(X_train)
+std = np.std(X_train)
+X_train = (X_train - mean) / std
+X_test = (X_test - mean) / std
+####################################
+
+
 
 model = Sequential()
-model.add(Conv2D(120, (3, 3), input_shape=(32, 32, 3), activation='relu') )
-model.add(Conv2D(130, (4, 4), activation='relu'))
-model.add(Conv2D(130, (4, 4), activation='relu'))
-model.add(Conv2D(150, (3, 3), activation='relu'))
-model.add(Conv2D(160, (3, 3), activation='relu'))
-model.add(Conv2D(130, (3, 3), activation='relu'))
+model.add(Conv2D(60, (2, 2), input_shape=(32, 32, 3), activation='relu') )
+model.add(Conv2D(130, (2, 2), activation='relu'))
+model.add(Conv2D(130, (2, 2), activation='relu'))
+model.add(Conv2D(150, (2, 2), activation='relu'))
+model.add(Conv2D(150, (2, 2), activation='relu'))
+model.add(Conv2D(150, (2, 2), activation='relu'))
+# model.add(Conv2D(160, (3, 3), activation='relu'))
+# model.add(Conv2D(130, (3, 3), activation='relu'))
 model.add(Flatten())
-model.add(Dense(100, activation='relu'))
+model.add(Dense(500, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(500, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(100, activation='softmax'))
 
@@ -45,7 +60,7 @@ model.summary()
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 es = EarlyStopping(monitor='val_loss', mode='min', patience=20, restore_best_weights=True, verbose=1)
-model.fit(X_train, y_train, epochs=1000, batch_size=500, validation_split=0.2, callbacks=[es], verbose=1)
+model.fit(X_train, y_train, epochs=1000, batch_size=500, validation_split=0.15, callbacks=[es], verbose=1)
 
 res = model.evaluate(X_test, y_test)
 
