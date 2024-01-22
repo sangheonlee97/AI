3a23a7434fe84f501c19ad7a66610dda1e82b286
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-01-22 17:51:45 +0900

    동일 데이터셋으로 dnn모델과 cnn모델 성능 비교

---
+++ keras/keras33_MaxPooling0.py
@@ -0,0 +1,54 @@
+from keras.datasets import fashion_mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Conv2D, Flatten, MaxPooling2D
+from keras.callbacks import EarlyStopping
+from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+(X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
+
+print(X_train.shape)
+print(X_test.shape)
+# print(pd.value_counts(y_train))
+# plt.imshow(X_train[5])
+# plt.show()
+
+X_train = X_train.reshape(-1, 28, 28, 1)
+X_test = X_test.reshape(-1, 28, 28, 1)
+
+print(y_train.shape)
+y_train = y_train.reshape(-1, 1)
+y_test = y_test.reshape(-1, 1)
+
+ohe = OneHotEncoder(sparse=False)
+y_train = ohe.fit_transform(y_train)
+y_test = ohe.transform(y_test)
+
+print(X_train.shape)
+print(X_test.shape)
+model = Sequential()
+model.add(Conv2D(150, (3,3), padding='same', strides=2, input_shape=(28,28,1), activation='relu'))
+model.add(MaxPooling2D())
+model.add(Conv2D(180, (3,3), padding='valid', activation='relu'))
+model.add(MaxPooling2D())
+model.add(Conv2D(180, (3,3), padding='same', activation='relu'))
+model.add(MaxPooling2D())
+model.add(Conv2D(180, (3,3), padding='same', activation='relu'))
+model.add(Conv2D(180, (3,3), padding='same', activation='relu'))
+model.add(Flatten())
+model.add(Dense(500))
+model.add(Dropout(0.5))
+model.add(Dense(500))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
+model.summary()
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+es = EarlyStopping(monitor='val_loss', mode='min', patience=50, verbose=1, restore_best_weights=True)
+model.fit(X_train, y_train, epochs=300, batch_size=500, callbacks=[es], validation_split=0.2)
+
+res = model.evaluate(X_test, y_test)
+
+print("acc : ", res[1])
---
+++ keras/keras34_dnn1_mnist.py
@@ -0,0 +1,55 @@
+from tensorflow.python.keras.layers import Conv2D, Dense, Flatten, Dropout
+import numpy as np
+import pandas as pd
+from keras.datasets import mnist
+from keras.models import Sequential
+from sklearn.preprocessing import OneHotEncoder
+
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+print(X_train.shape, y_train.shape)
+print(X_test.shape, y_test.shape)
+
+y_train = y_train.reshape(-1, 1)
+y_test = y_test.reshape( -1, 1)
+
+ohe = OneHotEncoder(sparse_output=False)
+y_train = ohe.fit_transform(y_train)
+y_test = ohe.transform(y_test)
+
+# print(X_train[0])       # 5
+# print(pd.value_counts(y_train))
+# print(np.unique(y_train, return_counts=True))
+
+X_train = X_train.reshape(60000, 28*28).astype('float32')
+# X_test = X_test.reshape(10000, 28, 28, 1)
+X_test = X_test.reshape(X_test.shape[0], X_test.shape[1]*X_test.shape[2]).astype('float32')
+print(X_train.shape, y_train.shape)
+print(X_test.shape, y_test.shape)
+
+#2 data
+model = Sequential()
+model.add(Dense(1000, input_shape=(784,), activation='relu'))
+model.add(Dense(3000, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(3000, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(3000, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(3000, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
+
+
+#3 compile, fit
+from keras.callbacks import EarlyStopping
+es = EarlyStopping(monitor='val_loss', mode='min', patience=30, verbose=1, restore_best_weights=True )
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(X_train, y_train, batch_size=500, verbose=1, epochs=10000, validation_split=0.2 , callbacks=[es])
+
+#3 evaluate, predict
+res = model.evaluate(X_test, y_test)
+print("loss : ", res[0])
+print("acc : ", res[1])
+####### 0.987(stride, padding)  ,  0.988(그냥),   0.97(dnn)
