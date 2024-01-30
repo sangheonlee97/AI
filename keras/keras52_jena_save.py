ad58c826495c8338b9f708cd03252e2b43b90315
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-01-30 19:06:38 +0900

    save 단계에서 데이터 전처리

--- keras/keras52_jena_save.py
+++ keras/keras52_jena_save.py
@@ -1,9 +1,46 @@
 import numpy as np
 import pandas as pd
-
+from sklearn.preprocessing import MinMaxScaler
 path = "..//_data//kaggle//jena//jena_climate_2009_2016.csv"
 
 dataset = pd.read_csv(path, index_col=0)
+
+######################################  이상치 처리
+# 이상치 처리 풍속 : 343580 ~ 343598 = 3.47 
+#       최대 풍속 : 343580 ~ 343598 = 4.98
+print(np.min(dataset['wv (m/s)']))
+print(np.min(dataset['max. wv (m/s)']))
+dataset.iloc[343577:343597, -3] = 3.47
+dataset.iloc[343577:343597, -2] = 4.98
+print(np.min(dataset['wv (m/s)']))
+print(np.min(dataset['max. wv (m/s)']))
+######################################
+
+######################################  정규분포 모양으로 전처리
+# sqrt_values = dataset['VPmax (mbar)']
+# # sqrt_values = np.sqrt(dataset['VPmax (mbar)'])
+
+# plt.hist(sqrt_values, bins=20, edgecolor='black')
+# plt.title('Histogram of sqrt(VPdef (mbar))')
+# plt.xlabel('sqrt(VPdef (mbar))')
+# plt.ylabel('Frequency')
+# plt.show()
+### VPdef (mbar), 'VPmax (mbar)' 컬럼은 log를 씌우는 편이 정규분포에 가까워진다
+print("@@@@@@@@@@@@@@@@@@@@@@@")
+print(np.max(dataset['VPmax (mbar)']))
+print(np.max(dataset['VPdef (mbar)']))
+dataset.iloc[:,5] = np.sqrt(dataset.iloc[:,5])
+dataset.iloc[:,7] = np.sqrt(dataset.iloc[:,7])
+print(np.max(dataset['VPmax (mbar)']))
+print(np.max(dataset['VPdef (mbar)']))
+######################################
+
+
+# scaling
+sc = MinMaxScaler()
+dataset = sc.fit_transform(dataset)
+dataset = pd.DataFrame(dataset)
+
 
 def split_Xy(data, size, col):
     X, y = [], []
@@ -13,7 +50,7 @@ def split_Xy(data, size, col):
         X.append(X_s)
         y.append(y_s)
     return np.array(X), np.array(y)
-timesteps = 4
+timesteps = 3
 target_col = 1
 X, y = split_Xy(dataset, timesteps, target_col)
 path2 = "..//_data//kaggle//jena//"
