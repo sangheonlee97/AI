feedf6f8ecdc2b61ae8a82d1093e9a5a72d18ca5
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-02-08 17:17:43 +0900

    kaggle obesity] base

---
+++ python/kaggle_obesity/kaggle_obesity_base_0.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+path = '..//_data//kaggle//obesity//'
+def oheconcat(data, col):
+    data = pd.DataFrame(data)
+    temp = data.iloc[:,col]
+    temp = pd.get_dummies(temp)
+    data = pd.DataFrame(data)
+    data = data.drop(data.columns[col], axis=1)
+    data = np.concatenate([data, temp], axis=1)
+    return data
+
+train_csv = pd.read_csv(path + "train.csv", index_col=0)
+test_csv = pd.read_csv(path + "test.csv", index_col=0)
+submission_csv = pd.read_csv(path + "sample_submission.csv")
+
+# 1. data
+X = train_csv.drop(['NObeyesdad'], axis=1)
+y = train_csv['NObeyesdad']
+# print(X, X.shape)   # (20758, 16)
+# print(y, y.shape)   # (20758, )
+
+# print(np.unique(y, return_counts=True)) # 7개, [2523, 3082, 2910, 3248, 4046, 2427, 2522]
+#       ['Insufficient_Weight', 'Normal_Weight', 'Obesity_Type_I',
+#        'Obesity_Type_II', 'Obesity_Type_III', 'Overweight_Level_I',
+#        'Overweight_Level_II']
+
+ohelist = [0, 4, 5, 9, 11, 15]
+lelist = [8, 14]
+# for col in lelist:
+#     X, test_csv = le(X, test_csv, col)
+mapping_dict = {'no': 0, 'Sometimes': 1, 'Frequently': 2, 'Always' :3}
+
+# 'Category' 열을 매핑하여 새로운 'Mapped' 열 생성
+for col in lelist:
+    X.iloc[:, col] = X.iloc[:, col].map(mapping_dict)
+    test_csv.iloc[:, col] = test_csv.iloc[:, col].map(mapping_dict)
+
+idxcheck = 0
+for col in ohelist:
+    X = oheconcat(X, col - idxcheck)
+    test_csv = oheconcat(test_csv, col - idxcheck)
+    idxcheck += 1
+
+print(X)
+
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
+
+
+# 2. model
+rfc = RandomForestClassifier()
+
+# 3. compile, fit
+rfc.fit(X_train, y_train)
+
+# 4. eval
+sc = rfc.score(X_test, y_test)
+print("score : ", sc)
+sub = rfc.predict(test_csv)
+sub = le.inverse_transform(sub)
+
+submission_csv['NObeyesdad'] = sub
+submission_csv.to_csv(path + "submission.csv", index=False)
