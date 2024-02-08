2f2a886e6bf1a71a62ddf8166352488a6e07900e
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-02-08 18:35:06 +0900

    ...

--- ml/m09_GridSearchCV_01.py
+++ ml/m09_GridSearchCV_01.py
@@ -2,6 +2,8 @@ import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.svm import SVC
 from sklearn.model_selection import StratifiedKFold, train_test_split, cross_val_score, cross_val_predict, GridSearchCV
+from sklearn.metrics import accuracy_score
+import time
 
 best_score = [0, 999, 999]
 X, y = load_iris(return_X_y=True)
@@ -16,10 +18,20 @@ parameters = [
 
 model = GridSearchCV(SVC(), parameters, cv=StratifiedKFold, verbose=1, n_jobs=10)
 
+starttime = time.time()
 model.fit(X_train, y_train)
-
+endtime = time.time()
 print("best 매개변수 : ", model.best_estimator_)
 print("best parameters : ", model.best_params_)
 
 print("best score : ", model.best_score_)
 print("model.score : ", model.score(X_test, y_test))
+
+y_pred = model.predict(X_test)
+print("best acc_score : ", accuracy_score(y_test, y_pred))
+y_pred_best = model.best_estimator_.predict(X_test)
+print("best tuned acc : ", accuracy_score(y_test, y_pred_best))
+print("time : {} 초".format(round(endtime - starttime, 2)))
+
+import pandas as pd
+print(pd.DataFrame(model.cv_results_))
--- python/연습장.py
+++ python/연습장.py
@@ -1,4 +1,11 @@
 import numpy as np
 import random
-for i in range(10):
-    print(i)
+x = np.array([0, 1, 1, 2, 2, 2, 3, 4, 4, 4, 4, 4])
+print("std: ",np.std(x))
+print("mean: ",np.mean(x))
+from sklearn.preprocessing import StandardScaler
+x = x.reshape(-1, 1)
+sc = StandardScaler()
+x = sc.fit_transform(x)
+
+print(np.unique(x, return_counts=True))
