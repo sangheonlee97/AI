83893bc0f5fb5b8c8646f179e741647671b44908
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-02-13 18:03:36 +0900

    pipeline 실습

---
+++ ml/m17_pipeline_2개.py
@@ -0,0 +1,34 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.pipeline import make_pipeline
+from sklearn.decomposition import PCA       # 차원 축소
+
+
+datasets = load_iris()
+X = datasets.data
+y = datasets.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y )
+
+# scaler = MinMaxScaler()
+# X_train = scaler.fit_transform(X_train)
+# X_test = scaler.transform(X_test)
+# model = RandomForestClassifier()
+
+model = make_pipeline(MinMaxScaler(), StandardScaler(), PCA(), RandomForestClassifier(min_samples_split=2, min_samples_leaf=10, random_state=42))
+
+
+
+model.fit(X_train,y_train)
+
+results = model.score(X_test, y_test)
+
+print("model : ", model, ", ",'score : ', results)
+y_pred = model.predict(X_test)
+
+acc = accuracy_score(y_test, y_pred)
+print("model : ", model, ", ","acc : ", acc)
+print("\n")
