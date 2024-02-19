61f809d7d4310435070a65816d15f5d2301740a0
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-02-19 11:19:23 +0900

    XGBoost 실습

--- python/kaggle_obesity/kaggle_obesity_base_0_smote.py
+++ python/kaggle_obesity/kaggle_obesity_base_0_smote.py
@@ -58,12 +58,14 @@ y = le.fit_transform(y)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
-smt = SMOTE(random_state=42, n_jobs=-3)
-X_train, y_train = smt.fit_resample(X_train, y_train)
-
+# smt = SMOTE(random_state=42, n_jobs=-3)
+# X_train, y_train = smt.fit_resample(X_train, y_train)
+print(X_train.shape, y_train.shape)
 
 # 2. model
 rfc = RandomForestClassifier()
+
+
 
 # 3. compile, fit
 rfc.fit(X_train, y_train)
