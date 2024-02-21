8502d2a30f005f3cc06629eb0e85ba75f42f502a
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-02-21 18:19:13 +0900

    ...

--- python/연습장.py
+++ python/연습장.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 # XLSX 파일 읽기
-xlsx_file_path = '한국마사회_경주상세정보(제주_부경)_20230720.xlsx'
+xlsx_file_path = 'KETI-2017-SL-Annotation-v2_1.xlsx'
 df = pd.read_excel(xlsx_file_path)
 
 # CSV 파일로 저장
-csv_file_path = '경마.csv'
+csv_file_path = 'KETI-2017-SL-Annotation-v2_1.csv'
 df.to_csv(csv_file_path, index=False)
