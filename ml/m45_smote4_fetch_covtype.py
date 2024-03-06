94afe0edd73d388a004beeebb3cd1facd93ed310
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-03-06 19:47:19 +0900

    Update m45_smote4_fetch_covtype.py

--- ml/m45_smote4_fetch_covtype.py
+++ ml/m45_smote4_fetch_covtype.py
@@ -10,14 +10,14 @@ X, y = fetch_covtype(return_X_y=True)
 print(X.shape)
 print(np.unique(y, return_counts=True))
 
-# X, _, y, _ = train_test_split(X, y, train_size=0.1, random_state=42, stratify=y)
+X, _, y, _ = train_test_split(X, y, train_size=0.1, random_state=42, stratify=y)
 print(np.unique(y, return_counts=True))
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 from imblearn.over_sampling import SMOTE
 
-smt = SMOTE()
-# X_train, y_train = smt.fit_resample(X_train, y_train)
+smt = SMOTE(random_state=42)
+X_train, y_train = smt.fit_resample(X_train, y_train)
 print(np.unique(y_train, return_counts=True))
 
 model = RandomForestClassifier(random_state=42)
@@ -28,9 +28,14 @@ f1 = f1_score(y_test, y_pred, average='macro')
 print("acc : ", model.score(X_test, y_test))
 print("f1 : ", f1)
 
+# 그냥
+# acc :  0.9533316695782381
+# f1 :  0.9240692940764312
 
+# 축소
 # acc :  0.8817657688667069
 # f1 :  0.8100147334457833
 
-# acc :  0.8871869890715085
-# f1 :  0.8428638676992627
+# 축소 + 증폭
+# acc :  0.8894243180449187
+# f1 :  0.8488403516407349
