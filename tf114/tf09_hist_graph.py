048e52a997971a48612c8acbcac450f928196dd9
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-03-28 16:06:15 +0900

    tensorflow1 시각화

---
+++ tf114/tf09_hist_graph.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+
+tf.set_random_seed(123)
+sess = tf.compat.v1.Session()
+
+X_data = [1, 2, 3, 4, 5]
+y_data = [3, 5, 7, 9, 11]
+X = tf.compat.v1.placeholder(dtype=tf.float32)
+y = tf.compat.v1.placeholder(dtype=tf.float32)
+
+w = tf.compat.v1.Variable(tf.random_normal([1]), dtype=tf.float32)
+b = tf.compat.v1.Variable(tf.random_normal([1]), dtype=tf.float32)
+
+
+hypothesis = X * w + b
+
+loss = tf.compat.v1.reduce_mean(tf.square(hypothesis - y))
+optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=0.001)
+train = optimizer.minimize(loss=loss)
+
+sess.run(tf.compat.v1.global_variables_initializer())
+losses = []
+weights = []
+biases = []
+for step in range(10000):
+    _, loss_val, w_val, b_val = sess.run([train, loss, w, b], feed_dict={X : X_data , y : y_data })
+    print("%-5s" % step, '\t',  "%-12s" % loss_val,'\t',  "%-10s" % w_val[0],'\t', "%-12s" %  b_val[0])
+    losses.append(loss_val)
+    weights.append(w_val)
+    biases.append(b_val)
+
+X_pred = [6, 7, 8]
+X_test = tf.compat.v1.placeholder(tf.float32, shape=[None])
+y_pred = X_test * w_val + b_val
+predict = sess.run(y_pred, feed_dict={X_test : X_pred})
+print('[6, 7, 8]의 예측값 : [{}, {}, {}]'.format(predict[0], predict[1], predict[2]))
+# print(losses)
+sess.close()
+import matplotlib.pyplot as plt
+plt.figure(figsize=(60,60))
+plt.subplot(131)
+plt.plot(losses)
+plt.xlabel('epoch')
+plt.ylabel('loss')
+
+plt.subplot(132)
+plt.plot(weights)
+plt.xlabel('epoch')
+plt.ylabel('weight')
+
+plt.subplot(133)
+plt.plot(biases)
+plt.xlabel('epoch')
+plt.ylabel('bias')
+
+plt.show()
---
+++ tf114/tf10_loss_weights_.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+tf.set_random_seed(777)
+
+X = [1, 2, 3]
+y = [1, 2, 3]
+
+w = tf.compat.v1.placeholder(tf.float32)
+
+hypothesis = X * w
+
+loss = tf.reduce_mean(tf.square(hypothesis - y))
+
+w_history = []
+loss_history = []
+
+with tf.compat.v1.Session() as sess:
+    for i in range(-30, 50):
+        curr_w = i
+        curr_loss = sess.run(loss, feed_dict={w:curr_w})
+        
+        w_history.append(curr_w)
+        loss_history.append(curr_loss)
+
+plt.plot(w_history, loss_history)
+plt.xlabel('w')
+plt.ylabel('loss')
+plt.show()
